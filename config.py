6fccad17d225818a063c621ec6dfa0d3d34336a2
Vecz <kopylov227@gmail.com>
2022-02-07 13:09:00 +0000

    fix

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-TOKEN = "1666594003:AAFhS1hzQM-9WGIidpA1Lygzgc1zOb731KM"
+TOKEN = ""
 config = {"port" : 8080, "database_url": "postgres://food_user:food_password@localhost/food", "require_ssl" : False}
