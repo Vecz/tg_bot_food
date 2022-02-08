f9f9a1681846c0f62de21cf8c57d06c618df342d
Vecz <kopylov227@gmail.com>
2022-02-08 12:59:08 +0000

    fix config again

--- config.py
+++ config.py
@@ -1,7 +1,2 @@
-<<<<<<< HEAD
 TOKEN = ""
 config = {"port" : 8080, "database_url": "postgres://food_user:food_password@localhost/food", "require_ssl" : False}
-=======
-TOKEN = ""
-config = {"port" : 8080, "database_url": "postgres://forum_user:forum_password@localhost/forum", "require_ssl" : False}
->>>>>>> 123170af52bd54858517939f119bb88c9a997d30
